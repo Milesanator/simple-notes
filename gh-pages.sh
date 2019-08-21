74d1eeea1f780460073e09103542497f84326a38
miles <miles.tuffs@qred.com>
2019-08-21 15:26:54 +0200

    :wrench: Add deploy script

---
+++ gh-pages.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash# Sync master branch and prepare staging branch.
+git checkout master
+git pull
+git checkout -B gh-pages-staging
+git branch -D gh-pages # Build storybook-static for staging and push to gh-pages.
+
+yarn build
+git add -f build
+git commit -m "Add gh-pages"
+git subtree split --prefix build -b gh-pages
+
+git push origin -f gh-pages:gh-pages # Clean up branches and files.
+git checkout master
+git branch -D gh-pages gh-pages-staging
