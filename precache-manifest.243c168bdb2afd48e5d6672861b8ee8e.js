a691dcb00b6edd0ca039865f311e1a9986313076
miles <miles.tuffs@qred.com>
2019-08-22 16:11:02 +0200

    Updates

---
+++ precache-manifest.243c168bdb2afd48e5d6672861b8ee8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f958a36e3f06279e818cb0d5ae00e45",
+    "url": "/simple-notes/index.html"
+  },
+  {
+    "revision": "d65351350cd4cb2b2667",
+    "url": "/simple-notes/static/css/main.e9ee0c84.chunk.css"
+  },
+  {
+    "revision": "ca86a102786ae9759d1d",
+    "url": "/simple-notes/static/js/2.2ca30864.chunk.js"
+  },
+  {
+    "revision": "d65351350cd4cb2b2667",
+    "url": "/simple-notes/static/js/main.155d0c4d.chunk.js"
+  },
+  {
+    "revision": "da0167c085960ec4ec82",
+    "url": "/simple-notes/static/js/runtime~main.6ac72d4c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64358bb6fc821c262f34047f232459c3.js"
+  "/simple-notes/precache-manifest.243c168bdb2afd48e5d6672861b8ee8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-notes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
